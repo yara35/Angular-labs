6e1e22e7ae59328dfff988db2ffffa081513187f
Yara Mohamed <myara1405@gmail.com>
2025-05-30 12:16:45 +0300

    projects-updates

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -11,14 +11,47 @@ export class Projects {
   projects = [
    {
       title: 'Space Zone',
-      image: 'assets/images/Home.png',
+      homeimg: 'assets/images/Home.png',
       description: 'Space Zone is a platform that allows users to book online the nearest available workspace based on their location. It simplifies the process of finding and reserving workspaces, offering a smooth and efficient experience for users looking to work in a shared environment.',
-      details: [
-        'Add and manage products with stock levels and categories.',
-        'Register suppliers and customers with full contact details.',
-        'Create purchase (in) and sales (out) bills easily.',
-        'Track payments, discounts, and taxes in bills.',
-        'View product and bill history with detailed reports.'
+      pages: [
+        {
+          pageimg:'assets/images/cards.png',
+          pagedes:"Showing the different workspace options available for booking.",
+          imgalt:'card'
+        },
+         {
+          pageimg:'assets/images/service.png',
+          pagedes:"Showing the services provided in each workspace.",
+          imgalt:'service'
+        },
+        {
+          pageimg:'assets/images/payment.png',
+          pagedes:"Showing the payment options for booking a workspace.",
+          imgalt:'payment'
+        }
+      ],
+      
+    },
+    {
+      title: 'Space Zone',
+      homeimg: 'assets/images/Home.png',
+      description: 'Space Zone is a platform that allows users to book online the nearest available workspace based on their location. It simplifies the process of finding and reserving workspaces, offering a smooth and efficient experience for users looking to work in a shared environment.',
+      pages: [
+        {
+          pageimg:'assets/images/cards.png',
+          pagedes:"Showing the different workspace options available for booking.",
+          imgalt:'card'
+        },
+         {
+          pageimg:'assets/images/service.png',
+          pagedes:"Showing the services provided in each workspace.",
+          imgalt:'service'
+        },
+        {
+          pageimg:'assets/images/payment.png',
+          pagedes:"Showing the payment options for booking a workspace.",
+          imgalt:'payment'
+        }
       ],
       
     },
